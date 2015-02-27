76c1127d35ddee92007417b26c98f7e0cd7ce033
Jahde <jahde@me.com>
2015-02-27 11:24:37 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kiyber::Application.config.secret_key_base = '33cde67df9625fd1ba02c81777f0b6b19b545452f8ae41e6c970d2188275639ad98d68087a0bb9d1349ee0b9ffaea6d38d0ddd1fe49ef5d47153a9d7a4c3cdb6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kiyber::Application.config.session_store :cookie_store, key: '_kiyber_session'
