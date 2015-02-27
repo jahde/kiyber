32f13508551e6c14a5130efe57d8d9dc53ff827f
Jahde <jahde@me.com>
2015-02-27 14:39:41 -0500

    Add pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Kiyber::Application.routes.draw do
   resources :items
 
   resources :services
+
+  get '/about', to: 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
